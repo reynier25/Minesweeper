cfc13781186891850ea6f459bb777332dc62db05
reynier25 <fsoto@uchicago.edu>
2019-11-25 10:55:36 -0800

    generate and populate board

--- board.rb
+++ board.rb
@@ -35,6 +35,29 @@ class Board
     @grid.flatten.all? { |tile| tile.bombed? != tile.explored? }
   end
 
+  private
 
+  def generate_board
+    @grid = Array.new(@grid_size) do |row|
+      Array.new(@grid_size) { |col| Tile.new(self, [row, col]) }
+    end
+
+    plant_bombs
+  end
+
+  def plant_bombs
+    total_bombs = 0
+    while total_bombs < @num_bombs
+      rand_pos = Array.new(2) { rand(@grid_size) }
+
+      tile = self[rand_pos]
+      next if tile.bombed?
+
+      tile.plant_bomb
+      total_bombs += 1
+    end
+
+    nil
+  end
 end
 
