efd74f044ec7e43603c3a58062d073a33f9ce8f0
reynier25 <fsoto@uchicago.edu>
2019-11-26 11:19:02 -0800

    create tile class

---
+++ tile.rb
@@ -0,0 +1,39 @@
+class Tile
+  DELTAS = [
+    [-1, -1],
+    [-1,  0],
+    [-1,  1],
+    [ 0, -1],
+    [ 0,  1],
+    [ 1, -1],
+    [ 1,  0],
+    [ 1,  1]
+  ].freeze
+
+  attr_reader :pos
+
+  def initialize(board, pos)
+    @board, @pos = board, pos
+    @bombed, @explored, @flagged = false, false, false
+  end
+
+  def bombed?
+    @bombed
+  end
+
+  def explored?
+    @explored
+  end
+
+  def flagged?
+    @flagged
+  end
+
+  def adjacent_bomb_count
+    neighbors.select(&:bombed?).count
+  end
+
+
+end
+
+
